f70a510f4de2c5db6c90f174d18ed3fc943bf18c
秦定六国 <1312941775@qq.com>
2022-03-03 16:10:16 +0800

    Create test_2022_1_24.c

---
+++ test_2022_1_24.c
@@ -0,0 +1,45 @@
+//
+// Created by 13129 on 2022/1/21.
+//
+#include <stdio.h>
+//int main()
+//{
+//    short s=0;
+//    int a=10;
+//    printf("%d",sizeof (s=a+5));
+//    printf("%d",s);
+//    return 0;
+//}
+//int test1(int arr[])
+//{
+//    printf("%d\n",sizeof (arr));
+//}
+//int test2(char ch[])
+//{
+//    printf("%d\n",sizeof (ch));
+//}
+//int main()
+//{
+//    int arr[10]={0};
+//    char ch[10]={0};
+//    printf("%d\n",sizeof (arr));
+//    printf("%d\n",sizeof (ch));
+//    test1(arr);
+//    test2(ch);
+//    return 0;
+//}
+//int main()
+//{
+//    int i=0,a=0,b=2,c=3,d=4;
+//    i=a++ && ++b && d++;
+//
+//    printf("%d %d %d %d\n",a,b,c,d);
+//    return 0;
+//}
+int main()
+{
+    int a=3;
+    int b=(a>5?3:-3);
+    printf("%d\n",b);0 `
+    return 0;
+}
